5f4ea1595192e01c8fa4d7343a0f196d413c8bef
Luthfi <luthfihadi543@gmail.com>
2025-09-28 11:56:59 +0700

    update

--- meet-4/my-minikit-app/next.config.ts
+++ meet-4/my-minikit-app/next.config.ts
@@ -5,7 +5,7 @@ const nextConfig = {
     return [
       {
         source: "/about",
-        destination: "/",
+        destination: "https://api.farcaster.xyz/miniapps/hosted-manifest/01998e9f-cc97-572c-017a-67f9a12a091a",
         permanent: true,
       },
     ];
